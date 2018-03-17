31124618129e633296f41ab983434f0f09d5acc8
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-03-17 19:06:45 +0200

    update

--- cli/Provisioner/PhpFpm.php
+++ cli/Provisioner/PhpFpm.php
@@ -29,30 +29,38 @@ class PhpFpm
         $this->apt->installQuietly("php7.2-fpm");
 
         $this->apt->cli->quietlyAsUser('
-        # Tweak Some PHP-FPM Settings
-
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.2/fpm/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/fpm/php.ini
+');
 
-# Configure FPM Pool Settings
+        (new Filesystem())->put('/etc/nginx/sites-available', '
+        server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-sed -i "s/^user = www-data/user = forge/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/^group = www-data/group = forge/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.owner.*/listen.owner = forge/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.group.*/listen.group = forge/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.2/fpm/pool.d/www.conf
+    root /var/www/html;
+    index index.php index.html index.htm index.nginx-debian.html;
 
-# Configure Primary Nginx Settings
+    server_name _;
 
-sed -i "s/user www-data;/user forge;/" /etc/nginx/nginx.conf
-sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
-sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
-sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
-        ');
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+');
+        (new Nginx($this->apt,$this->apt->cli))->restart();
 
         $this->restart();
     }
